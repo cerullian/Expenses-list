9b31b61414c80efb5066fb42a8fe3d345f67e2ba
Daniela Patrício <danielaccpatricio@gmail.com>
2023-03-29 17:29:38 +0100

    added expenses by month chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
-import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2022");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
                 selected={filteredYear}
                 onChangeFilter={filterChangeHandler}
             />
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList items={filteredExpenses} />
         </Card>
     );
